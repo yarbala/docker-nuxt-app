1cbcc4f09d621482ae003508d8f587967e2fd510
yarbala <yarbala@gmail.com>
2021-08-17 15:00:17 +0300

    python

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t yarbala/nuxt-app:latest -t yarbala/nuxt-app:v1.0.4 .
+docker build -t yarbala/nuxt-app:latest -t yarbala/nuxt-app:v1.0.5 .
 
-docker push yarbala/nuxt-app:v1.0.4
+docker push yarbala/nuxt-app:v1.0.5
