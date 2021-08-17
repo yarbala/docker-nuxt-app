e4981637fb271154cc24b820fdd853db56d68700
yarbala <yarbala@gmail.com>
2021-08-17 16:33:42 +0300

    node v12.14.0

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t yarbala/nuxt-app:latest -t yarbala/nuxt-app:v1.0.9 .
+docker build -t yarbala/nuxt-app:latest -t yarbala/nuxt-app:v1.0.10 .
 
-#docker push yarbala/nuxt-app:v1.0.9
+#docker push yarbala/nuxt-app:v1.0.10
